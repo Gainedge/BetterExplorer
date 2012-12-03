4345d69a910d304c91fafaffffb88cb2e6bb91a6
DimitarCC <dimitar_cenev@hotmail.com>
2012-12-03 09:24:22 +0200

    - Added custom Action project

---
+++ Installer/InstallCustom/Installer1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using Microsoft.Win32;
+using System.IO;
+
+namespace InstallCustom
+{
+    [RunInstaller(true)]
+    public partial class Installer1 : System.Configuration.Install.Installer
+    {
+        public Installer1()
+        {
+            InitializeComponent();
+        }
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Install(IDictionary stateSaver)
+        {
+            base.Install(stateSaver);
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Commit(IDictionary savedState)
+        {
+
+
+            base.Commit(savedState);
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Rollback(IDictionary savedState)
+        {
+            base.Rollback(savedState);
+        }
+
+        [System.Security.Permissions.SecurityPermission(System.Security.Permissions.SecurityAction.Demand)]
+        public override void Uninstall(IDictionary savedState)
+        {
+          RegistryKey rk = Registry.ClassesRoot;
+          RegistryKey rks = rk.OpenSubKey(@"Folder\shell\opennewwindow\command", true);
+          RegistryKey rksh = rk.OpenSubKey(@"Folder\shell", true);
+          rks.SetValue("DelegateExecute", "{11dbb47c-a525-400b-9e80-a54615a090c0}", RegistryValueKind.String);
+          rksh.SetValue("", "");
+          rks.Close();
+          rksh.Close();
+          rk.Close();
+            base.Uninstall(savedState);
+            
+
+        }
+
+    }
+}
