10d65e2c097b5394a78251801f94de4da2fd24e6
Jayke R. Huempfner <jacobrh94@hotmail.com>
2013-10-18 12:42:21 -0500

    Testing committing from Visual Studio 2013

--- WpfControlLibrary1/BreadcrumbBarFSItem.cs
+++ WpfControlLibrary1/BreadcrumbBarFSItem.cs
@@ -8,31 +8,31 @@ using System.Windows.Threading;
 
 namespace BetterExplorerControls
 {
-  public class BreadcrumbBarFSItem
-  {
-    public String DisplayName { get; set; }
-    public String RealPath { get; set; }
+    public class BreadcrumbBarFSItem
+    {
+        public String DisplayName { get; set; }
+        public String RealPath { get; set; }
 
-    public BreadcrumbBarFSItem(ShellObject fsItem)
-    {
-      this.DisplayName = fsItem.ParsingName;
-      if (fsItem.ParsingName.StartsWith(":"))
-      {
-        Thread t = new Thread(() =>
+        public BreadcrumbBarFSItem(ShellObject fsItem)
         {
-          foreach (ShellObject item in KnownFolders.All)
-          {
-            this.DisplayName = this.DisplayName.Replace(item.ParsingName, item.GetDisplayName(DisplayNameType.Default)).Replace(".library-ms", "");
-          }
-        });
-        t.Start();
-      }
-      this.RealPath = fsItem.ParsingName;
+            this.DisplayName = fsItem.ParsingName;
+            if (fsItem.ParsingName.StartsWith(":"))
+            {
+                Thread t = new Thread(() =>
+                {
+                    foreach (ShellObject item in KnownFolders.All)
+                    {
+                        this.DisplayName = this.DisplayName.Replace(item.ParsingName, item.GetDisplayName(DisplayNameType.Default)).Replace(".library-ms", "");
+                    }
+                });
+                t.Start();
+            }
+            this.RealPath = fsItem.ParsingName;
+        }
+        public BreadcrumbBarFSItem(String display, String real)
+        {
+            this.DisplayName = display;
+            this.RealPath = real;
+        }
     }
-    public BreadcrumbBarFSItem(String display, String real)
-    {
-      this.DisplayName = display;
-      this.RealPath = real;
-    }
-  }
 }
