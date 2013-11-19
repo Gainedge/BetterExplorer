2ce656f3d23f882a3e36a9c07d7b632ac2b707ec
Dimitar Tsenev <dimitarcenevjp@gmail.com>
2013-11-19 12:37:59 +0200

    - Chnaged version number

--- BExplorer/BetterExplorer/Properties/AssemblyInfo.cs
+++ BExplorer/BetterExplorer/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.0.5.37251")]
-[assembly: AssemblyFileVersion("2.0.5.37251")]
-[assembly: AssemblyInformationalVersion("2.0.5.130612.37251 Beta 3 Update 2")]
+[assembly: AssemblyVersion("2.5.0.22052")]
+[assembly: AssemblyFileVersion("2.5.0.22052")]
+[assembly: AssemblyInformationalVersion("2.5.0.131119.22052 Alpha 1")]
