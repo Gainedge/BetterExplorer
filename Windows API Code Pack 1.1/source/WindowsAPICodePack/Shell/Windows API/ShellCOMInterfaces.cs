0084eaa72ad30d75ccea96f756e0172bc0f3dd86
Aaron Campf <AaronCampf@Gmail.com>
2014-08-07 17:29:07 -0700

    Removed extra SICHINTF

--- Windows API Code Pack 1.1/source/WindowsAPICodePack/Shell/Windows API/ShellCOMInterfaces.cs
+++ Windows API Code Pack 1.1/source/WindowsAPICodePack/Shell/Windows API/ShellCOMInterfaces.cs
@@ -10,13 +10,15 @@ using Microsoft.WindowsAPICodePack.Taskbar;
 using MS.WindowsAPICodePack.Internal;
 
 namespace Microsoft.WindowsAPICodePack.Shell {
-	public enum SICHINTF {
-		SICHINT_DISPLAY = 0x00000000,
-		SICHINT_CANONICAL = 0x10000000,
-		SICHINT_TEST_FILESYSPATH_IF_NOT_EQUAL = 0x20000000,
-		SICHINT_ALLFIELDS = unchecked((int)0x80000000)
-	}
 
+	/*
+	//public enum SICHINTF {
+	//	SICHINT_DISPLAY = 0x00000000,
+	//	SICHINT_CANONICAL = 0x10000000,
+	//	SICHINT_TEST_FILESYSPATH_IF_NOT_EQUAL = 0x20000000,
+	//	SICHINT_ALLFIELDS = unchecked((int)0x80000000)
+	//}
+	*/
 	// Disable warning if a method declaration hides another inherited from a parent COM interface
 	// To successfully import a COM interface, all inherited methods need to be declared again with 
 	// the exception of those already declared in "IUnknown"
