cdfc2919a7aa540c495c9cacaf6822be09a2755b
Dimitar Tsenev <dimitarcenevjp@gmail.com>
2014-08-14 10:54:49 +0300

    - added few methods

--- Shell/Interop/Kernel32.cs
+++ Shell/Interop/Kernel32.cs
@@ -43,6 +43,50 @@ namespace BExplorer.Shell.Interop
 				[DllImport("kernel32", SetLastError = true, CharSet = CharSet.Unicode)]
 				public static extern IntPtr LoadLibrary(string lpFileName);
 
+				public enum FINDEX_INFO_LEVELS {
+					FindExInfoStandard = 0,
+					FindExInfoBasic = 1
+				}
+
+				public enum FINDEX_SEARCH_OPS {
+					FindExSearchNameMatch = 0,
+					FindExSearchLimitToDirectories = 1,
+					FindExSearchLimitToDevices = 2
+				}
+				[StructLayout(LayoutKind.Sequential, CharSet = CharSet.Auto)]
+				public struct WIN32_FIND_DATA {
+					public uint dwFileAttributes;
+					public System.Runtime.InteropServices.ComTypes.FILETIME ftCreationTime;
+					public System.Runtime.InteropServices.ComTypes.FILETIME ftLastAccessTime;
+					public System.Runtime.InteropServices.ComTypes.FILETIME ftLastWriteTime;
+					public uint nFileSizeHigh;
+					public uint nFileSizeLow;
+					public uint dwReserved0;
+					public uint dwReserved1;
+					[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 260)]
+					public string cFileName;
+					[MarshalAs(UnmanagedType.ByValTStr, SizeConst = 14)]
+					public string cAlternateFileName;
+				}
+
+				[DllImport("kernel32", CharSet = CharSet.Unicode)]
+				public static extern bool FindNextFile(IntPtr hFindFile, out WIN32_FIND_DATA lpFindFileData);
+
+				[DllImport("kernel32.dll", SetLastError = true)]
+				public static extern bool FindClose(IntPtr hFindFile);
+
+				public const int FIND_FIRST_EX_CASE_SENSITIVE = 1;
+				public const int FIND_FIRST_EX_LARGE_FETCH = 2;
+
+				[DllImport("kernel32.dll", SetLastError = true, CharSet = CharSet.Unicode)]
+				public static extern IntPtr FindFirstFileEx(
+						string lpFileName,
+						FINDEX_INFO_LEVELS fInfoLevelId,
+						out WIN32_FIND_DATA lpFindFileData,
+						FINDEX_SEARCH_OPS fSearchOp,
+						IntPtr lpSearchFilter,
+						int dwAdditionalFlags);
+
 				public static bool IsThis64bitProcess()
 				{
 					return (IntPtr.Size == 8);
