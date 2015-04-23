bfe0da3b2ade98956bb81da553e4d0ebff372e57
Dimitar Tsenev <dimitarcenevjp@gmail.com>
2015-04-23 08:44:59 +0300

    - fix for exception on dir creation implementation

--- Shell/ShellTreeViewEx.cs
+++ Shell/ShellTreeViewEx.cs
@@ -1124,7 +1124,7 @@ namespace BExplorer.Shell {
               //case ShellNotifications.SHCNE.SHCNE_CREATE:
                 try {
                   var sho = FileSystemListItem.ToFileSystemItem(IntPtr.Zero, info.Item1);
-                  if (!sho.Parent.ParsingName.Equals(KnownFolders.Network.ParsingName, StringComparison.InvariantCultureIgnoreCase))
+                  if (sho.Parent == null || !sho.Parent.ParsingName.Equals(KnownFolders.Network.ParsingName, StringComparison.InvariantCultureIgnoreCase))
                     break;
                   var existingItem = this.ShellTreeView.Nodes.OfType<TreeNode>().Last().Nodes.OfType<TreeNode>().Where(w => (w.Tag as IListItemEx) != null && (w.Tag as IListItemEx).ParsingName == sho.ParsingName).Count();
                   if (existingItem > 0)
@@ -1144,6 +1144,8 @@ namespace BExplorer.Shell {
                   this.ShellTreeView.Nodes.OfType<TreeNode>().Last().Nodes.Add(node);
                   //}
                 } catch (AccessViolationException) {
+
+                } catch (NullReferenceException) {
 
                 }
                 break;
