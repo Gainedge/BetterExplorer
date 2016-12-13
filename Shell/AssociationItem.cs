7989331346c8b60580043056bb1a20399fea53d2
Aaron Campf <aaroncampf@gmail.com>
2016-12-13 10:44:48 -0800

    Formatted to standard

--- Shell/AssociationItem.cs
+++ Shell/AssociationItem.cs
@@ -6,79 +6,68 @@ using System.IO;
 using System.Linq;
 using System.Windows.Media.Imaging;
 
-namespace BExplorer.Shell
-{
-    //TODO: Document this!
+namespace BExplorer.Shell {
+	//TODO: Document this!
 
-    public class AssociationItem
-    {
-        public ShellItem Owner { get; private set; }
-        public String DisplayName { get; set; }
-        public String ApplicationPath { get; set; }
-        public BitmapSource Icon => new ShellItem(this.ApplicationPath.ToShellParsingName()).Thumbnail.SmallBitmapSource;
+	public class AssociationItem {
+		public ShellItem Owner { get; private set; }
+		public String DisplayName { get; set; }
+		public String ApplicationPath { get; set; }
+		public BitmapSource Icon => new ShellItem(this.ApplicationPath.ToShellParsingName()).Thumbnail.SmallBitmapSource;
 
 
-        public AssociationItem(ShellItem item)
-        {
-            this.Owner = item;
-        }
+		public AssociationItem(ShellItem item) {
+			this.Owner = item;
+		}
 
 
-        public void Invoke()
-        {
-            RegistryKey root = Registry.ClassesRoot;
-            var applications = root.OpenSubKey("Applications", RegistryKeyPermissionCheck.ReadSubTree);
-            var currentApplication = applications.OpenSubKey(Path.GetFileName(this.ApplicationPath), RegistryKeyPermissionCheck.ReadSubTree);
+		public void Invoke() {
+			RegistryKey root = Registry.ClassesRoot;
+			var applications = root.OpenSubKey("Applications", RegistryKeyPermissionCheck.ReadSubTree);
+			var currentApplication = applications.OpenSubKey(Path.GetFileName(this.ApplicationPath), RegistryKeyPermissionCheck.ReadSubTree);
 
-            Action OnFail = () =>
-            {
-                Process.Start(this.ApplicationPath, "\"" + this.Owner.ParsingName + "\"");
-                applications.Close();
-                root.Close();
-            };
+			Action OnFail = () => {
+				Process.Start(this.ApplicationPath, "\"" + this.Owner.ParsingName + "\"");
+				applications.Close();
+				root.Close();
+			};
 
-            if (currentApplication == null)
-            {
-                OnFail();
-                return;
-            }
+			if (currentApplication == null) {
+				OnFail();
+				return;
+			}
 
-            var command = currentApplication.OpenSubKey(@"shell\open\command", RegistryKeyPermissionCheck.ReadSubTree);
-            if (command == null)
-            {
-                command = currentApplication.OpenSubKey(@"shell\edit\command", RegistryKeyPermissionCheck.ReadSubTree);
-            }
+			var command = currentApplication.OpenSubKey(@"shell\open\command", RegistryKeyPermissionCheck.ReadSubTree);
+			if (command == null) {
+				command = currentApplication.OpenSubKey(@"shell\edit\command", RegistryKeyPermissionCheck.ReadSubTree);
+			}
 
-            if (command == null)
-            {
-                OnFail();
-                return;
-            }
+			if (command == null) {
+				OnFail();
+				return;
+			}
 
-            var commandValue = command.GetValue("").ToString();
-            var argsArray = Shell32.CommandLineToArgs(commandValue).ToList();
-            var executable = argsArray[0];
-            argsArray.RemoveAt(0);
-            for (int i = 0; i < argsArray.Count; i++)
-            {
-                if (argsArray[i] != "%1" && argsArray[i] != "%L" && argsArray[i].ToLowerInvariant().Contains("photoviewer.dll"))
-                {
-                    argsArray[i] = "\"" + argsArray[i].Replace(",", "") + "\",";
-                }
-                else if ((argsArray[i] == "%1" || argsArray[i] == "%L") && !argsArray[i].ToLowerInvariant().Contains("photoviewer.dll"))
-                {
-                    argsArray[i] = "\"" + argsArray[i] + "\"";
-                }
-            }
+			var commandValue = command.GetValue("").ToString();
+			var argsArray = Shell32.CommandLineToArgs(commandValue).ToList();
+			var executable = argsArray[0];
+			argsArray.RemoveAt(0);
+			for (int i = 0; i < argsArray.Count; i++) {
+				if (argsArray[i] != "%1" && argsArray[i] != "%L" && argsArray[i].ToLowerInvariant().Contains("photoviewer.dll")) {
+					argsArray[i] = "\"" + argsArray[i].Replace(",", "") + "\",";
+				}
+				else if ((argsArray[i] == "%1" || argsArray[i] == "%L") && !argsArray[i].ToLowerInvariant().Contains("photoviewer.dll")) {
+					argsArray[i] = "\"" + argsArray[i] + "\"";
+				}
+			}
 
-            var args = String.Join(" ", argsArray).Replace("%1", this.Owner.ParsingName).Replace("%L", this.Owner.ParsingName).Replace(@"\\", @"\");
-            commandValue = Environment.ExpandEnvironmentVariables(commandValue);
-            Process.Start(executable, args);
+			var args = String.Join(" ", argsArray).Replace("%1", this.Owner.ParsingName).Replace("%L", this.Owner.ParsingName).Replace(@"\\", @"\");
+			commandValue = Environment.ExpandEnvironmentVariables(commandValue);
+			Process.Start(executable, args);
 
-            command.Close();
-            currentApplication.Close();
-            applications.Close();
-            root.Close();
-        }
-    }
+			command.Close();
+			currentApplication.Close();
+			applications.Close();
+			root.Close();
+		}
+	}
 }
