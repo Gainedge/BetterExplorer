99307f32b8029421b2ac79c9771179c7a959d59b
Aaron Campf <aaroncampf@gmail.com>
2016-12-23 13:47:46 -0800

    Cleaned and documented file

--- WpfControlLibrary1/ListviewColumnDropDown.cs
+++ WpfControlLibrary1/ListviewColumnDropDown.cs
@@ -1,33 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
 
 namespace BetterExplorerControls {
-  public class ListviewColumnDropDown : ContextMenu {
 
-    static ListviewColumnDropDown()
-    {
-        DefaultStyleKeyProperty.OverrideMetadata(
-            typeof(ListviewColumnDropDown),
-            new FrameworkPropertyMetadata(typeof(ListviewColumnDropDown)));
-			
-    }
-    public void AddItem(object Item) {
-      MenuItem mnuitem = new MenuItem();
-      mnuitem.IsCheckable = true;
-      mnuitem.Header = Item;
+	public class ListviewColumnDropDown : ContextMenu {
+		/// <summary>DefaultStyleKeyProperty.OverrideMetadata(typeof(ListviewColumnDropDown), new FrameworkPropertyMetadata(typeof(ListviewColumnDropDown)));</summary>
+		static ListviewColumnDropDown() {
+			DefaultStyleKeyProperty.OverrideMetadata(typeof(ListviewColumnDropDown), new FrameworkPropertyMetadata(typeof(ListviewColumnDropDown)));
+		}
 
-      this.Items.Add(mnuitem);
-      //CheckBox.Checked += CheckBox_Checked;
-      //CheckBox.Unchecked += CheckBox_Unchecked;
-
-      //sPanel.Children.Add(CheckBox);
-      //return this;
-    }
-
-  }
+		/// <summary>
+		/// Adds a new <see cref="MenuItem"/> to <see cref="Items"/> with <see cref="MenuItem.IsCheckable"/> = true and <see cref="MenuItem.Header"/> = Item
+		/// </summary>
+		/// <param name="Item">The item that will be put into the header</param>
+		public void AddItem(object Item) {
+			this.Items.Add(new MenuItem() { IsCheckable = true, Header = Item });
+		}
+	}
 }
