881f87402560b1f71fe15c1f2df19289caca5848
Aaron Campf <aaroncampf@gmail.com>
2016-12-23 14:10:58 -0800

    Commented out unused class

--- Shell/SyncQueue.cs
+++ Shell/SyncQueue.cs
@@ -98,99 +98,99 @@ namespace BExplorer.Shell {
 		public T Dequeue() => queue.Dequeue();
 	}
 
-	public class ConQueue<T> {
-#if ConcurrentQueue
+	//	public class ConQueue<T> {
+	//#if ConcurrentQueue
 
-		private readonly ConcurrentQueue<T> queue = new ConcurrentQueue<T>();
+	//		private readonly ConcurrentQueue<T> queue = new ConcurrentQueue<T>();
 
-		public void Clear() {
-			T Item;
-			while (queue.TryDequeue(out Item)) {
+	//		public void Clear() {
+	//			T Item;
+	//			while (queue.TryDequeue(out Item)) {
 
-			}
-		}
+	//			}
+	//		}
 
-		/// <summary>
-		/// Adds an object to the end of the System.Collections.Generic.Queue[T] then runs System.Threading.Monitor.PulseAll(queue) when queue.Count == 1;
-		/// </summary>
-		/// <param name="item">The object to add to the System.Collections.Generic.Queue[T]. The value can be null for reference types.</param>
-		public void Enqueue(T item) {
-			lock (queue) {
-				queue.Enqueue(item);
-				if (queue.Count == 1) {
-					// wake up any blocked dequeue
-					System.Threading.Monitor.PulseAll(queue);
-				}
-			}
-		}
+	//		/// <summary>
+	//		/// Adds an object to the end of the System.Collections.Generic.Queue[T] then runs System.Threading.Monitor.PulseAll(queue) when queue.Count == 1;
+	//		/// </summary>
+	//		/// <param name="item">The object to add to the System.Collections.Generic.Queue[T]. The value can be null for reference types.</param>
+	//		public void Enqueue(T item) {
+	//			lock (queue) {
+	//				queue.Enqueue(item);
+	//				if (queue.Count == 1) {
+	//					// wake up any blocked dequeue
+	//					System.Threading.Monitor.PulseAll(queue);
+	//				}
+	//			}
+	//		}
 
-		/// <summary>
-		/// Removes and returns the object at the beginning of the System.Collections.Generic.Queue(Of T). while (queue.Count == 0) System.Threading.Monitor.Wait(queue);
-		/// </summary>
-		/// <returns>
-		/// The object that is removed from the beginning of the System.Collections.Generic.Queue(Of T).
-		/// </returns>
-		/// <Exceptions>
-		/// System.InvalidOperationException: The System.Collections.Generic.Queue(Of T) is empty.
-		/// </Exceptions>
-		public T Dequeue() {
-			T Result;
-			lock (queue) {
-				if (queue.Count == 0) {
-					System.Threading.Monitor.Wait(queue);
-				}
-				if (!queue.TryDequeue(out Result)) {
-					//this.ToString();
+	//		/// <summary>
+	//		/// Removes and returns the object at the beginning of the System.Collections.Generic.Queue(Of T). while (queue.Count == 0) System.Threading.Monitor.Wait(queue);
+	//		/// </summary>
+	//		/// <returns>
+	//		/// The object that is removed from the beginning of the System.Collections.Generic.Queue(Of T).
+	//		/// </returns>
+	//		/// <Exceptions>
+	//		/// System.InvalidOperationException: The System.Collections.Generic.Queue(Of T) is empty.
+	//		/// </Exceptions>
+	//		public T Dequeue() {
+	//			T Result;
+	//			lock (queue) {
+	//				if (queue.Count == 0) {
+	//					System.Threading.Monitor.Wait(queue);
+	//				}
+	//				if (!queue.TryDequeue(out Result)) {
+	//					//this.ToString();
 
-					//throw new ApplicationException("Why!!");
-				}
-			}
+	//					//throw new ApplicationException("Why!!");
+	//				}
+	//			}
 
-			return Result;
-		}
+	//			return Result;
+	//		}
 
-#else
-		public readonly Queue<T> queue = new Queue<T>();
+	//#else
+	//		public readonly Queue<T> queue = new Queue<T>();
 
-		public void Clear() {
-			this.queue.Clear();
-		}
+	//		public void Clear() {
+	//			this.queue.Clear();
+	//		}
 
-		/// <summary>
-		/// Adds an object to the end of the System.Collections.Generic.Queue<T> then runs System.Threading.Monitor.PulseAll(queue) when queue.Count == 1;
-		/// </summary>
-		/// <param name="item">The object to add to the System.Collections.Generic.Queue<T>. The value can be null for reference types.</param>
-		public void Enqueue(T item) {
-			lock (queue) {
-				queue.Enqueue(item);
+	//		/// <summary>
+	//		/// Adds an object to the end of the System.Collections.Generic.Queue<T> then runs System.Threading.Monitor.PulseAll(queue) when queue.Count == 1;
+	//		/// </summary>
+	//		/// <param name="item">The object to add to the System.Collections.Generic.Queue<T>. The value can be null for reference types.</param>
+	//		public void Enqueue(T item) {
+	//			lock (queue) {
+	//				queue.Enqueue(item);
 
-				if (queue.Count == 1) {
-					// wake up any blocked dequeue
-					System.Threading.Monitor.PulseAll(queue);
-				}
-			}
-		}
+	//				if (queue.Count == 1) {
+	//					// wake up any blocked dequeue
+	//					System.Threading.Monitor.PulseAll(queue);
+	//				}
+	//			}
+	//		}
 
-		/// <summary>
-		/// Removes and returns the object at the beginning of the System.Collections.Generic.Queue(Of T). while (queue.Count == 0) System.Threading.Monitor.Wait(queue);
-		/// </summary>
-		/// <returns>
-		/// The object that is removed from the beginning of the System.Collections.Generic.Queue(Of T).
-		/// </returns>
-		/// <Exceptions>
-		/// System.InvalidOperationException: The System.Collections.Generic.Queue(Of T) is empty.
-		/// </Exceptions>
-		public T Dequeue() {
-			lock (queue) {
-				while (queue.Count == 0) {
-					// wait for item
-					System.Threading.Monitor.Wait(queue);
-				}
+	//		/// <summary>
+	//		/// Removes and returns the object at the beginning of the System.Collections.Generic.Queue(Of T). while (queue.Count == 0) System.Threading.Monitor.Wait(queue);
+	//		/// </summary>
+	//		/// <returns>
+	//		/// The object that is removed from the beginning of the System.Collections.Generic.Queue(Of T).
+	//		/// </returns>
+	//		/// <Exceptions>
+	//		/// System.InvalidOperationException: The System.Collections.Generic.Queue(Of T) is empty.
+	//		/// </Exceptions>
+	//		public T Dequeue() {
+	//			lock (queue) {
+	//				while (queue.Count == 0) {
+	//					// wait for item
+	//					System.Threading.Monitor.Wait(queue);
+	//				}
 
-				return queue.Dequeue();
-			}
-		}
-#endif
-	}
+	//				return queue.Dequeue();
+	//			}
+	//		}
+	//#endif
+	//	}
 
 }
