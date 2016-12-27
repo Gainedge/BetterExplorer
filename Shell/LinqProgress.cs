954a0d3eb08cbd4d3b292036ba02509fc1c1239c
Aaron Campf <aaroncampf@gmail.com>
2016-12-26 16:24:15 -0800

    Commented out unused code

--- Shell/LinqProgress.cs
+++ Shell/LinqProgress.cs
@@ -1,47 +1,48 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace BExplorer.Shell {
+//namespace BExplorer.Shell {
 
-	public class ProgressArgs<T> : EventArgs {
-		public T Item { get; private set; }
+//	/
+//	public class ProgressArgs<T> : EventArgs {
+//		public T Item { get; private set; }
 
-		public ProgressArgs(T item) {
-			this.Item = item;
-		}
-	}
+//		public ProgressArgs(T item) {
+//			this.Item = item;
+//		}
+//	}
 
-	//public class ProgressContext<T> : IEnumerable<T> {
-	//	private IEnumerable<T> source;
+//	public class ProgressContext<T> : IEnumerable<T> {
+//		private IEnumerable<T> source;
 
-	//	public ProgressContext(IEnumerable<T> source) {
-	//		this.source = source;
-	//	}
+//		public ProgressContext(IEnumerable<T> source) {
+//			this.source = source;
+//		}
 
-	//	public event EventHandler<ProgressArgs<T>> UpdateProgress;
+//		public event EventHandler<ProgressArgs<T>> UpdateProgress;
 
-	//	protected virtual void OnUpdateProgress(T item) {
-	//		EventHandler<ProgressArgs<T>> handler = this.UpdateProgress;
-	//		if (handler != null)
-	//			handler(this, new ProgressArgs<T>(item));
-	//	}
+//		protected virtual void OnUpdateProgress(T item) {
+//			EventHandler<ProgressArgs<T>> handler = this.UpdateProgress;
+//			if (handler != null)
+//				handler(this, new ProgressArgs<T>(item));
+//		}
 
-	//	public IEnumerator<T> GetEnumerator() {
-	//		//int count = 0;
-	//		foreach (var item in source) {
-	//			// The yield holds execution until the next iteration,
-	//			// so trigger the update event first.
-	//			OnUpdateProgress(item);
-	//			yield return item;
-	//		}
-	//	}
+//		public IEnumerator<T> GetEnumerator() {
+//			//int count = 0;
+//			foreach (var item in source) {
+//				// The yield holds execution until the next iteration,
+//				// so trigger the update event first.
+//				OnUpdateProgress(item);
+//				yield return item;
+//			}
+//		}
 
-	//	IEnumerator IEnumerable.GetEnumerator() {
-	//		return GetEnumerator();
-	//	}
-	//}
-}
+//		IEnumerator IEnumerable.GetEnumerator() {
+//			return GetEnumerator();
+//		}
+//	}
+//}
