0d5a6cbb8e2b605a011519ef542155a39da1ea49
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2017-11-04 17:24:19 +0200

    - some refactoring

--- Shell/SyncQueue.cs
+++ Shell/SyncQueue.cs
@@ -1,21 +1,21 @@
-﻿#define ConcurrentQueue
+﻿namespace BExplorer.Shell {
 
-using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+  using System;
+  using System.Collections.Generic;
 
-namespace BExplorer.Shell {
-
+  /// <summary>
+  /// A Queue for sync actions
+  /// </summary>
+  /// <typeparam name="T">The type of the elements in queue</typeparam>
   [System.Diagnostics.DebuggerStepThrough]
   public class SyncQueue<T> {
-    private readonly Queue<T> queue = new Queue<T>();
-    public void Clear() => this.queue.Clear();
-    public int Count() => this.queue.Count;
-    public Boolean Contains(T item) => queue.Contains(item);
+    private readonly Queue<T> _Queue = new Queue<T>();
 
+    public void Clear() => this._Queue.Clear();
+
+    public Int32 Count() => this._Queue.Count;
+
+    public Boolean Contains(T item) => this._Queue.Contains(item);
 
     /// <summary>
     /// Adds an object to the end of the System.Collections.Generic.Queue[T] then runs System.Threading.Monitor.PulseAll(queue) when queue.Count == 1;
@@ -23,13 +23,13 @@ namespace BExplorer.Shell {
     /// <param name="item">The object to add to the System.Collections.Generic.Queue[T]. The value can be null for reference types.</param>
     /// <param name="force">Force the item to update even if it has queue already contains it</param>
     public void Enqueue(T item, Boolean force = false) {
-      lock (queue) {
-        if (force || !queue.Contains(item)) {
-          queue.Enqueue(item);
+      lock (this._Queue) {
+        if (force || !this._Queue.Contains(item)) {
+          this._Queue.Enqueue(item);
 
-          if (queue.Count == 1) {
+          if (this._Queue.Count == 1) {
             // wake up any blocked dequeue
-            System.Threading.Monitor.PulseAll(queue);
+            System.Threading.Monitor.PulseAll(this._Queue);
           }
         }
       }
@@ -46,153 +46,14 @@ namespace BExplorer.Shell {
     /// System.InvalidOperationException: The System.Collections.Generic.Queue(Of T) is empty.
     /// </Exceptions>
     public T Dequeue() {
-      lock (queue) {
-        while (queue.Count == 0) {
+      lock (this._Queue) {
+        while (this._Queue.Count == 0) {
           // wait for item
-          System.Threading.Monitor.Wait(queue);
+          System.Threading.Monitor.Wait(this._Queue);
         }
 
-        return queue.Dequeue();
+        return this._Queue.Dequeue();
       }
     }
   }
-
-  public class QueueEx<T> {
-    private readonly Queue<T> queue = new Queue<T>();
-    /// <summary>Removes all objects</summary>
-    public void Clear() => this.queue.Clear();
-
-    /// <summary>Gets the number of elements contained</summary>
-    public int Count() => this.queue.Count;
-
-    /// <summary>
-    /// Determines whether an element is in the System.Collections.Generic.Queue`1.
-    /// </summary>
-    /// <param name="item">The object to locate in the System.Collections.Generic.Queue`1. The value can be null for reference types.</param>
-    /// <returns>true if item is found in the System.Collections.Generic.Queue`1; otherwise, false.</returns>
-    public Boolean Contains(T item) => queue.Contains(item);
-
-    /// <summary>
-    /// Removes and returns the object at the beginning of the System.Collections.Generic.Queue(Of T). while (queue.Count == 0) System.Threading.Monitor.Wait(queue);
-    /// </summary>
-    /// <returns>
-    /// The object that is removed from the beginning of the System.Collections.Generic.Queue(Of T).
-    /// </returns>
-    /// <Exceptions>
-    /// System.InvalidOperationException: The System.Collections.Generic.Queue(Of T) is empty.
-    /// </Exceptions>
-    public T Dequeue() => queue.Dequeue();
-
-    /// <summary>
-    /// Adds an object to the end of the System.Collections.Generic.Queue[T] then runs System.Threading.Monitor.PulseAll(queue) when queue.Count == 1;
-    /// </summary>
-    /// <param name="item">The object to add to the System.Collections.Generic.Queue[T]. The value can be null for reference types.</param>
-    /// <param name="force">Force the item to update even if it has queue already contains it</param>
-    public Boolean Enqueue(T item, Boolean force = false) {
-      if (!queue.Contains(item) || force) {
-        queue.Enqueue(item);
-        return true;
-      } else {
-        return false;
-      }
-    }
-
-
-  }
-
-  //	public class ConQueue<T> {
-  //#if ConcurrentQueue
-
-  //		private readonly ConcurrentQueue<T> queue = new ConcurrentQueue<T>();
-
-  //		public void Clear() {
-  //			T Item;
-  //			while (queue.TryDequeue(out Item)) {
-
-  //			}
-  //		}
-
-  //		/// <summary>
-  //		/// Adds an object to the end of the System.Collections.Generic.Queue[T] then runs System.Threading.Monitor.PulseAll(queue) when queue.Count == 1;
-  //		/// </summary>
-  //		/// <param name="item">The object to add to the System.Collections.Generic.Queue[T]. The value can be null for reference types.</param>
-  //		public void Enqueue(T item) {
-  //			lock (queue) {
-  //				queue.Enqueue(item);
-  //				if (queue.Count == 1) {
-  //					// wake up any blocked dequeue
-  //					System.Threading.Monitor.PulseAll(queue);
-  //				}
-  //			}
-  //		}
-
-  //		/// <summary>
-  //		/// Removes and returns the object at the beginning of the System.Collections.Generic.Queue(Of T). while (queue.Count == 0) System.Threading.Monitor.Wait(queue);
-  //		/// </summary>
-  //		/// <returns>
-  //		/// The object that is removed from the beginning of the System.Collections.Generic.Queue(Of T).
-  //		/// </returns>
-  //		/// <Exceptions>
-  //		/// System.InvalidOperationException: The System.Collections.Generic.Queue(Of T) is empty.
-  //		/// </Exceptions>
-  //		public T Dequeue() {
-  //			T Result;
-  //			lock (queue) {
-  //				if (queue.Count == 0) {
-  //					System.Threading.Monitor.Wait(queue);
-  //				}
-  //				if (!queue.TryDequeue(out Result)) {
-  //					//this.ToString();
-
-  //					//throw new ApplicationException("Why!!");
-  //				}
-  //			}
-
-  //			return Result;
-  //		}
-
-  //#else
-  //		public readonly Queue<T> queue = new Queue<T>();
-
-  //		public void Clear() {
-  //			this.queue.Clear();
-  //		}
-
-  //		/// <summary>
-  //		/// Adds an object to the end of the System.Collections.Generic.Queue<T> then runs System.Threading.Monitor.PulseAll(queue) when queue.Count == 1;
-  //		/// </summary>
-  //		/// <param name="item">The object to add to the System.Collections.Generic.Queue<T>. The value can be null for reference types.</param>
-  //		public void Enqueue(T item) {
-  //			lock (queue) {
-  //				queue.Enqueue(item);
-
-  //				if (queue.Count == 1) {
-  //					// wake up any blocked dequeue
-  //					System.Threading.Monitor.PulseAll(queue);
-  //				}
-  //			}
-  //		}
-
-  //		/// <summary>
-  //		/// Removes and returns the object at the beginning of the System.Collections.Generic.Queue(Of T). while (queue.Count == 0) System.Threading.Monitor.Wait(queue);
-  //		/// </summary>
-  //		/// <returns>
-  //		/// The object that is removed from the beginning of the System.Collections.Generic.Queue(Of T).
-  //		/// </returns>
-  //		/// <Exceptions>
-  //		/// System.InvalidOperationException: The System.Collections.Generic.Queue(Of T) is empty.
-  //		/// </Exceptions>
-  //		public T Dequeue() {
-  //			lock (queue) {
-  //				while (queue.Count == 0) {
-  //					// wait for item
-  //					System.Threading.Monitor.Wait(queue);
-  //				}
-
-  //				return queue.Dequeue();
-  //			}
-  //		}
-  //#endif
-  //	}
-
 }
