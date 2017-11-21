daced49a253c0fc74a26a4d785fc8175afa0e138
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2017-11-21 09:15:26 +0200

    - some formatting changes
    - fixed colors in Light theme

--- BExplorer/BetterExplorer/Properties/AssemblyInfo.cs
+++ BExplorer/BetterExplorer/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.4.34582")]
-[assembly: AssemblyFileVersion("2.5.4.34582")]
-[assembly: AssemblyInformationalVersion("2.5.4.171104.58.2 RC4")]
+[assembly: AssemblyVersion("2.5.17325.0711")]
+[assembly: AssemblyFileVersion("2.5.17325.0711")]
+[assembly: AssemblyInformationalVersion("2.5.4.171121.61.1 RC5")]
