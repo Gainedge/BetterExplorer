1715432a15e91a8e577c9812d9da964bb4974213
Dimitar Tsenev <dimitar_tsenev@outlook.com>
2018-06-06 11:43:01 +0200

    - updated some nuget packages
    - removed automatic versions

--- BExplorer/BetterExplorer/Properties/AssemblyInfo.cs
+++ BExplorer/BetterExplorer/Properties/AssemblyInfo.cs
@@ -51,6 +51,6 @@ using System.Windows;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("2.5.18156.0903")]
-[assembly: AssemblyFileVersion("2.5.18156.0903")]
+[assembly: AssemblyVersion("2.5.18157.0935")]
+[assembly: AssemblyFileVersion("2.5.18157.0935")]
 [assembly: AssemblyInformationalVersion("2.5.4.18085.0732.62.1 RC5")]
